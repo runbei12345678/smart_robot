b28cb1245eebc31102156669dba16c907eca1303
runbei12345678 <438084318@qq.com>
2026-03-08 21:03:08 +0800

    第一个测试github的文件

---
+++ ESP32_textgithub_2026001/ESP32_textgithub_2026001.ino
@@ -0,0 +1,18 @@
+void setup() 
+{
+  // put your setup code here, to run once:
+  pinMode(0,OUTPUT);
+  digitalWrite(0,LOW);
+
+}
+
+void loop() 
+{
+  // put your main code here, to run repeatedly:
+  digitalWrite(0,HIGH);
+  delay(500);
+  digitalWrite(0,LOW);
+  delay(500);
+
+
+}
